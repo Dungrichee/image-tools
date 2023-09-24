fa487eb3d9c1a5f61cd48bb75de99c77d6aac689
Dungrichee <dungriche@gmail.com>
2023-09-24 16:16:11 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    apps: [
+        {
+            script: 'npm start',
+        },
+    ],
+
+    deploy: {
+        production: {
+            key: 'img-aws-key.pem',
+            user: 'ubuntu',
+            host: '13.40.127.40',
+            ref: 'origin/main',
+            repo: 'https://github.com/Dungrichee/image-tools.git',
+            path: '/hom/ubuntu',
+            'pre-deploy-local': '',
+            'post-deploy':
+                'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': '',
+            ssh_options: 'ForwardAgent=yes',
+        },
+    },
+};

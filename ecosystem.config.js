469fd23ac29031f660abd533fe284b6f59b09349
Dungrichee <dungriche@gmail.com>
2023-09-24 22:31:13 +0700

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
             host: '13.40.127.40',
             ref: 'origin/main',
             repo: 'https://github.com/Dungrichee/image-tools.git',
-            path: '/hom/ubuntu',
+            path: '/home/ubuntu',
             'pre-deploy-local': '',
             'post-deploy':
                 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',

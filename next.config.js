28e6fb30390f619975a1b89c234e9a26d3f4fa6c
Dungrichee <dungriche@gmail.com>
2023-10-01 16:20:40 +0700

    fix cors

--- next.config.js
+++ next.config.js
@@ -1,24 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/Nutlope/restorePhotos",
-        permanent: false,
-      },
-      {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/ai-photo-restorer",
-        permanent: false,
-      },
-    ];
-  },
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    // images: {
+    //   domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
+    // },
+    // async redirects() {
+    //   return [
+    //     {
+    //       source: "/github",
+    //       destination: "https://github.com/Nutlope/restorePhotos",
+    //       permanent: false,
+    //     },
+    //     {
+    //       source: "/deploy",
+    //       destination: "https://vercel.com/templates/next.js/ai-photo-restorer",
+    //       permanent: false,
+    //     },
+    //   ];
+    // },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: '/api/v1/gfpgan',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
